f4c45d05247ab6bf970c47f846cd1d7085aa533d
D3nnEr <dennercroco@gmail.com>
2022-10-17 22:08:57 -0300

    CONCATENAÇÃO

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,30 @@
+public class Operadores {
+    
+    public static void main(String[] args) {
+        String concatenação = "?";
+
+        concatenação = 1+1+1+"1";
+
+        System.out.println(concatenação);
+
+        concatenação = 1+"1"+1+1;
+
+        System.out.println(concatenação);
+
+        concatenação = 1+"1"+1+"1";
+
+        System.out.println(concatenação);
+
+        concatenação = "1"+1+1+1;
+
+        System.out.println(concatenação);
+
+        concatenação = "1"+(1+1+1);
+
+        System.out.println(concatenação);
+
+
+
+
+    }
+}
